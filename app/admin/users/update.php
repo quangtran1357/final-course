610d94c3a1e01d0940d0e1722410ce105c362ae8
nhatquang <quangtran1357@gmail.com>
2017-11-16 13:41:59 +0700

    Finish admin products

--- app/admin/users/update.php
+++ app/admin/users/update.php
@@ -1,13 +1,16 @@
 <?php session_start(); ?>
 <?php require_once "../../../db/mysql.php"; ?>
-<?php require_once "../../helper/catalogs-helper.php"; ?>
+<?php require_once "../../helper/user-helper.php"; ?>
 <?php
-  if(isset($_POST["id"]) && isset($_POST["name"]) && isset($_POST["description"])){
+  if(isset($_POST["id"]) && isset($_POST["name"]) && isset($_POST["email"])
+    && isset($_POST["password"]) && isset($_POST["role"])){
     $id = $_POST["id"];
     $name = $_POST["name"];
-    $description = $_POST["description"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $role = $_POST["role"];
 
-    $sql = "update catalogs set name='$name',description ='$description' where id=$id";
+    $sql = "update users set name='$name',email ='$email',password='$password',role=$role where id=$id";
     $result = $conn->query($sql);
     if($result){
       $_SESSION["flash"] = "Updated success";
